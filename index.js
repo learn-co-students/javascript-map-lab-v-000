ae9a9087898fc5aa824e0c06dc7eb5b79e0d8cb7
johnram528 <johnram528@gmail.com>
2017-02-23 23:00:19 -0500

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,21 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+var sortedRobots = robots.map(function (robot){
+  if (knownDecepticons.includes(robot['name'])) {
+    var updated = Object.assign({}, robot, { alliance: 'decepticon' });
+  } else {
+    var updated = Object.assign({}, robot, { alliance: 'autobot' });
+  }
+  return updated
+})
+
+var coloredZebraStripes = zebraStripes.map(function(stripe, index){
+  if(index % 2 == 0){
+    var updated = Object.assign({}, stripe, {color: 'black'})
+  }else{
+    var updated = Object.assign({}, stripe, {color: 'white'})
+  }
+  return updated
+})
