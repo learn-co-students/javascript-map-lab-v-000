429c3cc83fefd42c9615144fdd9aee4637e706f3
SiamKing <thesiameseelectric@gmail.com>
2017-03-17 12:51:20 -0700

    yeah yeah yeahs

--- index.js
+++ index.js
@@ -21,3 +21,32 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var sortedRobots = robots.map(function(robot) {
+
+  if (knownDecepticons.includes(robot.name)) {
+    return Object.assign({}, robot, {
+      name: robot.name,
+      alliance: 'decepticon'
+    })
+  } else {
+    return Object.assign({}, robot, {
+      name: robot.name,
+      alliance: 'autobot'
+    })
+  }
+})
+
+var coloredZebraStripes = zebraStripes.map(function(stripe, i) {
+  if (i % 2 === 0) {
+    return Object.assign({}, stripe, {
+      width: stripe.width,
+      color: 'black'
+    })
+  } else {
+    return Object.assign({}, stripe, {
+      width: stripe.width,
+      color: 'white'
+    })
+  }
+})
