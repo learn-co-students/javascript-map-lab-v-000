c40b33d8664bbf219d86c660ff2ee37cca753487
WilliamMena <williammena1991@gmail.com>
2017-05-03 00:06:27 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(function (robot)
+{ 
+  if (knownDecepticons.includes(robot.name)) {
+    // console.log(robot + " decepticon")
+    return Object.assign({}, robot, {
+      alliance: 'decepticon'
+    })
+  } else {
+    // console.log(robot + " autobot")
+    return Object.assign({}, robot, {
+      alliance: 'autobot'
+    })
+  }
+})
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +36,16 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes = zebraStripes.map(function (stripe, index)
+{
+  if (index % 2 == 0){
+    return Object.assign({}, stripe, {
+      color: 'black'
+    })
+  } else {
+    return Object.assign({}, stripe, {
+      color: 'white'
+    })
+  }
+})
