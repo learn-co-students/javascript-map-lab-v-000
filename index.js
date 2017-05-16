b802683be0ae08eca230ba272975815590eaaa18
bentonwong <bentonwong@gmail.com>
2017-05-16 14:49:13 -0700

    done

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map(function (robot) {
+  return Object.assign({}, robot, {
+    alliance: knownDecepticons.includes(robot.name) ? "decepticon" : "autobot"
+  });
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +27,9 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var coloredZebraStripes = zebraStripes.map(function (zebraStripe, i) {
+  return Object.assign({}, zebraStripe, {
+    color: i % 2 ? "black" : "white"
+  });
+});
