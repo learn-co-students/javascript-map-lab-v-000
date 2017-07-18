19fa0dcfe7148ae5ed7267d911e555564cdfb076
Parv Kumar <pkumar@trinity.edu>
2017-07-18 21:29:10 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map(function(robot){
+    if (knownDecepticons.includes(robot.name)){
+      return Object.assign({}, robot, {
+      alliance:"decepticon"
+      })
+
+    }else{
+      return Object.assign({}, robot, {
+      alliance: "autobot"}
+  );
+};
+})
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +34,17 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var coloredZebraStripes = zebraStripes.map(function callback(zebrastripe, index, array){
+  if(index % 2 == 0){
+    return Object.assign({}, zebrastripe, {
+      color: "black"
+    });
+
+  }else{
+    return Object.assign({}, zebrastripe, {
+      color: "white"
+    });
+  }
+
+})
