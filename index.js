9432a8007cfa46f459e77ecd7b7dbca215ab573d
machen2 <victoriamachen@gmail.com>
2017-07-28 23:42:56 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+// my code part 1:
+var sortedRobots = robots.map(function (robot) {
+  const isDecepticon = knownDecepticons.includes(robot.name) // const because it goes out of scope with each iteration and wont change in each specific iteration
+  return Object.assign({}, robot, { alliance: isDecepticon ? 'decepticon' : 'autobot' })
+})
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +27,8 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+// my code part 2:
+var coloredZebraStripes = zebraStripes.map(function (element, index) {
+  return Object.assign({}, element, { color: index % 2 === 0 ? 'black' : 'white' })
+})
