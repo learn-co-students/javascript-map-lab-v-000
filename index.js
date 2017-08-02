01170ac1daafbdd4f7b28a2179c1eb6f8e760270
bshap93 <bshap93@gmail.com>
2017-08-02 17:37:45 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map(function(autobot) {
+  var isDecepticon = knownDecepticons.includes(autobot.name)
+
+  return Object.assign({}, autobot, {
+    alliance: isDecepticon ? 'decepticon' : 'autobot'
+  })
+})
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +29,11 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var coloredZebraStripes = zebraStripes.map(function(stripe, index) {
+  var isEven = (index % 2 === 0);
+
+  return Object.assign({}, stripe, {
+    color: isEven ? 'black' : 'white'
+  });
+});
