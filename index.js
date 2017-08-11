fae73e9d111103ff6bb5cb2fd738d7f3de7f4652
ruuuh20 <pkim2320@gmail.com>
2017-08-11 13:37:13 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,38 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+/* var sortedRobots = robots.map(robot => {
+  for(let i = 0, i < knownDecepticons.length, i++) {
+    if(knownDecepticons[i] === robot["name"]) {
+      return Object.assign({}, robot, {alliance: 'decepticon'})
+    }
+  }
+  return Object.assign({}, robot, {alliance: 'autobot'})
+})
+*/
+
+/* const sortedRobots = robots.map(function(robot) {
+   if (knownDecepticons.includes(robot.name)){
+     return Object.assign({}, robot, {
+       alliance: 'decepticon'
+     });
+  } else {
+     return Object.assign({}, robot, {
+       alliance: 'autobot'
+     });
+   }
+ });
+ */
+
+ var sortedRobots = robots.map(robot => {
+   const decepticon = knownDecepticons.includes(robot.name);
+   return Object.assign({}, robot, {
+     alliance: decepticon ? 'decepticon' : 'autobot'
+   });
+
+ });
+
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +53,23 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+
+
+// var new_array = arr.map(function callback(currentValue, index, array) {
+//     // Return element for new_array
+// }[, thisArg])
+
+
+var coloredZebraStripes = zebraStripes.map((stripe, index) => {
+  if  (index % 2 === 0) {
+    return Object.assign({}, stripe, {
+      color: 'black'
+    });
+  } else {
+    return Object.assign({}, stripe, {
+      color: 'white'
+    });
+  }
+
+});
