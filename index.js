8d8c9dfa43b6bca0c2a20e8f5140a5fed5166682
sarjumulmi <mulmi.sarju@gmail.com>
2017-08-12 13:33:47 -0400

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const sortedRobots = robots.map(function (robot) {
+  return Object.assign({}, robot, {
+    name: robot.name,
+    alliance: knownDecepticons.includes(robot.name) ? 'decepticon' : 'autobot'
+  });
+})
+// console.log(sortedRobots);
+
+const coloredZebraStripes = zebraStripes.map(function(zebraStripe, i) {
+  return Object.assign({}, zebraStripe, {
+    width: zebraStripe.width,
+    color: i%2===0 ? 'black' : 'white'
+  });
+});
