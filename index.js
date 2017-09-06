2225f17ac1432217c40cb95c5f3eca37ab252bcc
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-06 16:35:18 -0700

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+
+
+var sortedRobots = robots.map(function(bot){
+  if(knownDecepticons.includes(bot.name)){
+    return Object.assign({}, bot, {alliance: 'decepticon'})
+  }else {
+    return Object.assign({}, bot, {alliance: 'autobot'})
+  }
+})
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +31,11 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var coloredZebraStripes = zebraStripes.map(function(zebra,index){
+  if(parseInt(index,10) % 2 == 0){
+    return Object.assign({}, zebra, {color: 'black'})
+  }else{
+    return Object.assign({}, zebra, {color: 'white'})
+  }
+})
